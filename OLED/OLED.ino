884e72eb532eee138b804af0cfe63a136a9434a0
unknown <rahimasselah@gmail.com>
2025-01-12 20:09:48 +0200

    Print Hello world using OLED based on the SH1106
    - Had to use the U8g2lib, the Adafruit one is not compatible

---
+++ OLED/OLED.ino
@@ -0,0 +1,31 @@
+/**
+We will use the u8G2 library as it is the one compatible with our OLED display microcontroller (SH1106)
+**/
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+
+
+void setup(void) {
+  u8g2.begin();
+}
+
+void loop(void) {
+  u8g2.clearBuffer();					// clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+  u8g2.drawStr(0,10,"Hello World!");	// write something to the internal memory
+  u8g2.sendBuffer();					// transfer internal memory to the display
+  delay(1000);  
+}
+
