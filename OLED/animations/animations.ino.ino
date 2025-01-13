4e9d015480039d74e5f5517ea4e9c555f593a456
unknown <rahimasselah@gmail.com>
2025-01-13 20:54:13 +0200

    Working with OLED display based on the SH1106
    - Drawing Lines and text
    - Making animation

--- OLED/animations/animations.ino.ino
+++ OLED/animations/animations.ino.ino
@@ -12,37 +12,41 @@ We will use the u8G2 library as it is the one compatible with our OLED display m
 #include <Wire.h>
 #endif
 
-
-int h, w;
-
+const int animationDelay = 100;
+int currentTime = 0;
+int previousTime = 0;
+int startCol = 128;
+int offset = 0;
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-
-
-
 
 void setup(void) {
   u8g2.begin();
   u8g2.clearBuffer();					// clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
-  h = u8g2.getHeight();
-  w = u8g2.getWidth();
+  int h = u8g2.getHeight();
+  int w = u8g2.getWidth();
 }
 
 void loop(void) {
 
-  u8g2.clearBuffer();
+  currentTime = millis();
+  if ((currentTime - previousTime) > animationDelay)
+  {
+    previousTime = currentTime;
+    u8g2.clearBuffer();
+    u8g2.drawStr(startCol-offset, 32, "scrollingText"); // scroll to the left
+   
+    offset += 2;
+    if((startCol - offset) <= 0)
+    {
+      startCol = 128;
+      offset = 0;
+    }
   
-  //draw 2 diagonal lines
-  u8g2.drawLine(0,0, w -1, h -1);
-  u8g2.drawLine(w - 1, 0, 0, h - 1);
-  //draw center lines
-  u8g2.drawLine((w - 1)/2, 0, (w - 1)/2, h - 1);
-  u8g2.drawLine(0, (h - 1)/2, (w - 1), (h - 1)/2);
-
-
-  u8g2.sendBuffer();  
+  u8g2.sendBuffer();			
       
   }
-
+ 
+}
 
